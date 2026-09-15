277f04a9979477236c2c759afb9da8656437e9d5
hanamedo30-oss <hanamedo30@gmail.com>
2026-09-15 19:38:13 +0300

    task dj
    task 2

--- myproject/app2/views.py
+++ myproject/app2/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.http import HttpResponse
 
 def home_app2(request):
-    return HttpResponse("<h1>Welcome to Application 2</h1>")
+    return render(request, 'app2.html')
